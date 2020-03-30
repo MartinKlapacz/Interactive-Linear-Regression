5b3875dafcd441131b4dbd673712180f3dc79174
Martin Klapacz <martin.klapacz@redpoint.teseon.com>
2020-03-30 16:08:32 +0200

    graph is plotted correctly

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set_xlim([0, 10])
 ax.set_ylim([0, 10])
+regression_x = np.linspace(0, 10, 100)
 
 points = []
-regression_x = np.arange(0, 10, 0.1)
+
+cursor = Cursor(ax,
+    horizOn=True,
+    vertOn=True,
+    color='green',
+    linewidth=1.0)
 
 def compute_alpha_beta(points):
     x_values = list(map(lambda point: point[0], points))
@@ -19,9 +25,13 @@ def compute_alpha_beta(points):
 
     x_mean = np.mean(x_values)
     y_mean = np.mean(y_values)
+    
+    Sxy = np.sum(list(map(lambda point: (point[0] - x_mean) * (point[1] - y_mean), points)))
+    Sxx = np.sum(list(map(lambda point: (point[0] - x_mean) * (point[0] - x_mean), points)))
 
-    alpha = np.sum(list(map(lambda point: (point[0] - x_mean) * (point[1] - y_mean), points))) / np.sum(list(map(lambda point: (point[0] - x_mean)**2, points)))
-    beta = y_mean - alpha * x_mean
+    beta = Sxy / Sxx
+    alpha = y_mean - beta * x_mean
+
     return alpha, beta
 
 def linear_regression(alpha, beta, x):
@@ -31,18 +41,18 @@ def linear_regression(alpha, beta, x):
 def onclick(event):
     circle = plt.Circle((event.xdata, event.ydata), 0.1,  color='b')
     ax.add_artist(circle)
-    points.append([event.xdata, event.ydata])
+    points.append((event.xdata, event.ydata))
 
     if len(points) > 1:
         alpha, beta = compute_alpha_beta(points=points)
-        regression_curve = list(map(lambda x: (x, linear_regression(alpha, beta, x)), regression_x))
-        print(points)
-        plt.plot(regression_curve)
-    
+        line = plt.plot(regression_x, list(map(lambda x: linear_regression(alpha, beta, x), regression_x)), linestyle='-')    
     fig.canvas.draw()
 
+
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
 # both axes equally scaled
 plt.gca().set_aspect('equal', adjustable='box')
+
 plt.show()
 
